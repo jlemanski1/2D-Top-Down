eabebcf4720f4b42ae8c12577b4fc7315fd67d8e
Jonathan L <jlemanski1@gmail.com>
2018-09-07 11:40:37 -0600

    Adding the game as it is to repository

---
+++ 2D TopDown/Assets/Scripts/CameraController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Handles following the player and smoothing the camera
+/// </summary>
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+    private Vector3 targetPos;
+
+    [Header("Camera Settings")]
+    [SerializeField]
+    private Vector3 offset;     // Offset between camera center and player
+    public float interpVelocity;
+    public float minDistance;
+    public float followDistance;
+    
+
+	void Start () {
+        targetPos = transform.position;
+	}
+	
+	
+	void FixedUpdate () {
+        if (player) {
+            Vector3 posNoZ = transform.position;
+            posNoZ.z = player.transform.position.z;
+
+            Vector3 targetDirection = (player.transform.position - posNoZ);
+            interpVelocity = targetDirection.magnitude * 5f;
+            targetPos = transform.position + (targetDirection.normalized * interpVelocity * Time.deltaTime);
+            transform.position = Vector3.Lerp(transform.position, targetPos + offset, 0.25f);
+        }
+	}
+}
---
+++ 2D TopDown/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Handles Player Movement & Interaction
+/// </summary>
+
+public class PlayerController : MonoBehaviour {
+    [Header("Player Attributes")]
+    public float defaultSpeed;
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private float sprintSpeed;
+    
+    
+    private Vector2 moveDirection;
+
+    public Animator animator;
+
+
+    private void Awake () {
+        speed = defaultSpeed;
+        moveDirection = Vector2.zero;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetInput();
+        Move();
+        
+	}
+
+
+    public void Move() {
+        transform.Translate(moveDirection * speed * Time.deltaTime);
+    }
+
+
+    /// <summary>
+    /// Gets Keyboard Input
+    /// </summary>
+    private void GetInput() {
+        float moveH = Input.GetAxisRaw("Horizontal");
+        float moveV = Input.GetAxisRaw("Vertical");
+        
+        moveDirection = Vector2.zero;
+        // Reset Animation to Idle
+        animator.SetBool("North", false);
+        animator.SetBool("South", false);
+        animator.SetBool("West", false);
+        animator.SetBool("East", false);
+
+        // Move Up
+        if (moveV == 1) {
+                moveDirection += Vector2.up;
+                animator.SetBool("North", true);
+
+        }
+
+        // Move Left
+        else if (moveH == -1) {
+                moveDirection += Vector2.left;
+                animator.SetBool("West", true);     
+        }
+
+        // Move Down
+        else if (moveV == -1) {
+                moveDirection += Vector2.down;
+                animator.SetBool("South", true);
+
+        }
+
+        // Move Right
+        else if (moveH == 1) {
+                moveDirection += Vector2.right;
+                animator.SetBool("East", true);
+
+
+        }
+
+        // Sprint
+        if (Input.GetKey(KeyCode.LeftShift)) {
+            speed = sprintSpeed;
+            animator.speed = 1.1f;
+        }
+        // Return to walk
+        else if (Input.GetKeyUp(KeyCode.LeftShift)) {
+            speed = defaultSpeed;
+            animator.speed = 1;
+
+        }
+    }
+
+}

fbd77f404bbf6fa636680124154c21021dc74a24
Jonathan L <jlemanski1@gmail.com>
2018-09-12 10:13:22 -0600

    Fixed Sign issues
    - Signs and their UI Popups are now working as intended in their basic form

--- 2D TopDown/Assets/Scripts/Interactions/SignInteraction.cs
+++ 2D TopDown/Assets/Scripts/Interactions/SignInteraction.cs
@@ -7,24 +7,34 @@ public class SignInteraction : MonoBehaviour {
     [SerializeField] private GameObject panel;
     [SerializeField] private Text signText;
     [SerializeField] private Text interactText;
-    private BoxCollider2D sign;
-    private GameObject player;
+    private Collider2D sign;
+    private bool inRange;
 
 	void Start () {
-        sign = GetComponent<BoxCollider2D>();
-        player = GameObject.FindGameObjectWithTag("Player");
+        sign = GetComponent<Collider2D>();
+        inRange = false;
     }
 
 
     /// <summary>
-    /// Triggers the text box when the player is in range, and chooses to interact
+    /// Triggers the text box when the player is in range
     /// </summary>
     private void OnTriggerEnter2D(Collider2D col) {
         if (col.tag == "Player") {
+            inRange = true;
             signUI.SetActive(true);
-            interactText.text = "Press E to interact";
-            
-            if (Input.GetAxis("Interact") >= 1) {
+            Debug.Log("Player within range");
+            interactText.text = "Press E to interact"; 
+        }
+    }
+
+
+    /// <summary>
+    /// Opens the sign text box when the player interacts
+    /// </summary>
+    private void OnTriggerStay2D(Collider2D col) {
+        if (inRange) {
+            if (Input.GetKeyDown(KeyCode.E)) {
                 interactText.text = "";
                 panel.SetActive(true);
             }
@@ -32,11 +42,13 @@ public class SignInteraction : MonoBehaviour {
     }
 
 
+
     /// <summary>
     /// Closes the text box when the player strays from the sign
     /// </summary>
     private void OnTriggerExit2D(Collider2D col) {
         if (col.tag == "Player") {
+            inRange = false;
             panel.SetActive(false);
             signUI.SetActive(false);
         }
